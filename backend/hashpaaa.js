c5e95eeb060390eccb261a0c207c3cc54386df14
Akash pal <157100586+AKASHPAL1234@users.noreply.github.com>
2026-01-17 01:10:05 +0530

    Remove hash-password.js functionality

--- backend/hashpaaa.js
+++ backend/hashpaaa.js
@@ -1,9 +1,1 @@
-// hash-password.js
-import bcrypt from "bcryptjs";
 
-const plain = "12Akash@"; // yahan apna admin ka plain password do
-const saltRounds = 10;
-
-bcrypt.hash(plain, saltRounds).then(hash => {
-  console.log("HASH:", hash);
-}).catch(err => console.error(err));
